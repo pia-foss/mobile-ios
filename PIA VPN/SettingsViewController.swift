8c0be449fd507c6a12a270d999a664a9fa489c08
Davide De Rosa <keeshux@gmail.com>
2018-04-12 11:02:13 +0200

    Move killswitch last in section

--- PIA VPN/SettingsViewController.swift
+++ PIA VPN/SettingsViewController.swift
@@ -458,14 +458,14 @@ class SettingsViewController: AutolayoutViewController {
         }
         if Flags.shared.enablesMACESetting {
             rowsBySection[.applicationSettings] = [
-                .automaticReconnection,
                 .darkTheme,
+                .automaticReconnection,
                 .mace
             ]
         } else {
             rowsBySection[.applicationSettings] = [
-                .automaticReconnection,
-                .darkTheme
+                .darkTheme,
+                .automaticReconnection
             ]
         }
         if !Flags.shared.enablesContentBlockerSetting {
