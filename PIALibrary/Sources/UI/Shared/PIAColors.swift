5450683ca497ed525c1d6c4cac3961ef6ea0ea30
ueshiba <ueshiba@gmail.com>
2018-09-21 13:23:36 +0200

    Update colours and text styles

--- PIALibrary/Sources/UI/Shared/PIAColors.swift
+++ PIALibrary/Sources/UI/Shared/PIAColors.swift
@@ -12,27 +12,27 @@ import UIKit
 
 extension UIColor {
     
-    class var grey15: UIColor {
+    class var grey10: UIColor {
         return UIColor(red: 34.0 / 255.0, green: 37.0 / 255.0, blue: 46.0 / 255.0, alpha: 1.0)
     }
     
-    class var grey18: UIColor {
+    class var grey7: UIColor {
         return UIColor(red: 43.0 / 255.0, green: 46.0 / 255.0, blue: 57.0 / 255.0, alpha: 1.0)
     }
     
-    class var grey20: UIColor {
+    class var grey6: UIColor {
         return UIColor(red: 50.0 / 255.0, green: 54.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
     }
     
-    class var grey40: UIColor {
+    class var grey5: UIColor {
         return UIColor(red: 92.0 / 255.0, green: 99.0 / 255.0, blue: 112.0 / 255.0, alpha: 1.0)
     }
     
-    class var grey55: UIColor {
+    class var grey4: UIColor {
         return UIColor(red: 136.0 / 255.0, green: 144.0 / 255.0, blue: 153.0 / 255.0, alpha: 1.0)
     }
     
-    class var grey85: UIColor {
+    class var grey2: UIColor {
         return UIColor(red: 215.0 / 255.0, green: 216.0 / 255.0, blue: 217.0 / 255.0, alpha: 1.0)
     }
     
@@ -40,7 +40,7 @@ extension UIColor {
         return UIColor(white: 1.0, alpha: 1.0)
     }
     
-    class var grey92: UIColor {
+    class var grey1: UIColor {
         return UIColor(white: 238.0 / 255.0, alpha: 1.0)
     }
     
@@ -72,4 +72,44 @@ extension UIColor {
         return UIColor(red: 178.0 / 255.0, green: 53.0 / 255.0, blue: 45.0 / 255.0, alpha: 1.0)
     }
     
+    class var greenlight: UIColor {
+        return UIColor(red: 122.0 / 255.0, green: 250.0 / 255.0, blue: 120.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var greenDark30: UIColor {
+        return UIColor(red: 64.0 / 255.0, green: 153.0 / 255.0, blue: 61.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var yellowLight: UIColor {
+        return UIColor(red: 249.0 / 255.0, green: 208.0 / 255.0, blue: 1.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var orrange: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 166.0 / 255.0, blue: 35.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var black: UIColor {
+        return UIColor(white: 0.0, alpha: 1.0)
+    }
+    
+    class var grey12: UIColor {
+        return UIColor(red: 25.0 / 255.0, green: 27.0 / 255.0, blue: 33.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey11: UIColor {
+        return UIColor(red: 31.0 / 255.0, green: 33.0 / 255.0, blue: 42.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey8: UIColor {
+        return UIColor(red: 41.0 / 255.0, green: 44.0 / 255.0, blue: 56.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey3: UIColor {
+        return UIColor(red: 147.0 / 255.0, green: 155.0 / 255.0, blue: 163.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var transparentGrey: UIColor {
+        return UIColor(red: 147.0 / 255.0, green: 155.0 / 255.0, blue: 163.0 / 255.0, alpha: 0.13)
+    }
+
 }
--- PIALibrary/Sources/UI/iOS/Styles/TextStyles.swift
+++ PIALibrary/Sources/UI/iOS/Styles/TextStyles.swift
@@ -16,7 +16,7 @@ protocol TextStyling {
 public extension TextStyle {
     
     public static let textStyle1 = TextStyle(
-        font: UIFont.regularFontWith(size: 14),
+        font: UIFont.regularFontWith(size: 18),
         color: UIColor.white,
         foregroundColor: nil,
         backgroundColor: nil,
@@ -25,8 +25,8 @@ public extension TextStyle {
     )
 
     public static let textStyle2 = TextStyle(
-        font: UIFont.regularFontWith(size: 14),
-        color: UIColor.grey55,
+        font: UIFont.regularFontWith(size: 18),
+        color: UIColor.grey6,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
@@ -34,8 +34,8 @@ public extension TextStyle {
     )
 
     public static let textStyle3 = TextStyle(
-        font: UIFont.regularFontWith(size: 14),
-        color: UIColor.greenDark20,
+        font: UIFont.regularFontWith(size: 18),
+        color: UIColor.grey6,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
@@ -43,8 +43,8 @@ public extension TextStyle {
     )
 
     public static let textStyle4 = TextStyle(
-        font: UIFont.regularFontWith(size: 14),
-        color: UIColor.red,
+        font: UIFont.regularFontWith(size: 18),
+        color: UIColor.white,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
@@ -53,25 +53,25 @@ public extension TextStyle {
 
     public static let textStyle5 = TextStyle(
         font: UIFont.regularFontWith(size: 14),
-        color: UIColor.grey20,
-        foregroundColor: nil,
-        backgroundColor: nil,
-        tintColor: nil,
-        lineHeight: 18
-    )
-
-    public static let textStyle6 = TextStyle(
-        font: UIFont.regularFontWith(size: 12),
-        color: UIColor.white,
+        color: UIColor.greenDark20,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
         lineHeight: 16
     )
 
+    public static let textStyle6 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.red,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+
     public static let textStyle7 = TextStyle(
-        font: UIFont.regularFontWith(size: 12),
-        color: UIColor.grey55,
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.white,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
@@ -79,15 +79,78 @@ public extension TextStyle {
     )
 
     public static let textStyle8 = TextStyle(
-        font: UIFont.regularFontWith(size: 12),
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.grey6,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+
+    public static let textStyle9 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
         color: UIColor.greenDark20,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+
+    public static let textStyle10 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.white,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+
+    public static let textStyle11 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.white,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 22
+    )
+
+    public static let textStyle12 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.grey4,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+
+    public static let textStyle13 = TextStyle(
+        font: UIFont.regularFontWith(size: 14),
+        color: UIColor.grey6,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 22
+    )
+
+    public static let textStyle14 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.grey6,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
         lineHeight: 16
     )
 
-    public static let textStyle9 = TextStyle(
+    public static let textStyle15 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.grey4,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+    
+    public static let textStyle16 = TextStyle(
         font: UIFont.regularFontWith(size: 12),
         color: UIColor.red,
         foregroundColor: nil,
@@ -95,17 +158,71 @@ public extension TextStyle {
         tintColor: nil,
         lineHeight: 16
     )
-
-    public static let textStyle10 = TextStyle(
+    
+    public static let textStyle17 = TextStyle(
         font: UIFont.regularFontWith(size: 12),
-        color: UIColor.grey20,
+        color: UIColor.greenDark20,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 16
+    )
+    
+    public static let textStyle18 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.white,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 16
+    )
+    
+    public static let textStyle19 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.grey4,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 20
+    )
+    
+    public static let textStyle20 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.grey4,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
         lineHeight: 16
     )
 
-    public static let textStyle11 = TextStyle(
+    public static let textStyle21 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.grey4,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+    
+    public static let textStyle22 = TextStyle(
+        font: UIFont.regularFontWith(size: 12),
+        color: UIColor.black,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 18
+    )
+    
+    public static let textStyle23 = TextStyle(
+        font: UIFont.regularFontWith(size: 10),
+        color: UIColor.grey4,
+        foregroundColor: nil,
+        backgroundColor: nil,
+        tintColor: nil,
+        lineHeight: 15
+    )
+    
+    public static let textStyle24 = TextStyle(
         font: UIFont.regularFontWith(size: 10),
         color: UIColor.white,
         foregroundColor: nil,
@@ -113,17 +230,17 @@ public extension TextStyle {
         tintColor: nil,
         lineHeight: 15
     )
-
-    public static let textStyle12 = TextStyle(
+    
+    public static let textStyle25 = TextStyle(
         font: UIFont.regularFontWith(size: 10),
-        color: UIColor.grey55,
+        color: UIColor.grey6,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
         lineHeight: 15
     )
-
-    public static let textStyle13 = TextStyle(
+    
+    public static let textStyle26 = TextStyle(
         font: UIFont.regularFontWith(size: 10),
         color: UIColor.greenDark20,
         foregroundColor: nil,
@@ -131,19 +248,10 @@ public extension TextStyle {
         tintColor: nil,
         lineHeight: 15
     )
-
-    public static let textStyle14 = TextStyle(
+    
+    public static let textStyle27 = TextStyle(
         font: UIFont.regularFontWith(size: 10),
         color: UIColor.red,
-        foregroundColor: nil,
-        backgroundColor: nil,
-        tintColor: nil,
-        lineHeight: 15
-    )
-
-    public static let textStyle15 = TextStyle(
-        font: UIFont.regularFontWith(size: 10),
-        color: UIColor.grey20,
         foregroundColor: nil,
         backgroundColor: nil,
         tintColor: nil,
