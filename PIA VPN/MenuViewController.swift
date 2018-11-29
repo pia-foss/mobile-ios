f9bbd9ce62695c3cbaa9e43b444b26ae38523d4e
Jose Blaya <ueshiba@gmail.com>
2018-11-29 11:29:28 +0100

    - Remove password from AccountViewController - Use publicUsername instead of the token username in AccountViewController and MenuViewController

--- PIA VPN/MenuViewController.swift
+++ PIA VPN/MenuViewController.swift
@@ -124,7 +124,7 @@ class MenuViewController: AutolayoutViewController {
         assert(Client.providers.accountProvider.isLoggedIn, "Menu visible while not logged in")
 
         currentUser = Client.providers.accountProvider.currentUser
-        labelUsername.text = currentUser?.credentials.username
+        labelUsername.text = Client.providers.accountProvider.publicUsername ?? ""
         labelUsername.accessibilityLabel = L10n.Menu.Accessibility.loggedAs(currentUser?.credentials.username ?? "")
     }
     
