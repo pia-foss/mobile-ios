34c774278e268a18c0d95b1b911c35c07f5704c9
Jose Blaya <ueshiba@gmail.com>
2019-01-18 10:36:46 +0100

    Clean unused theme methods

--- PIALibrary/Sources/UI/iOS/Theme+LightPalette.swift
+++ PIALibrary/Sources/UI/iOS/Theme+LightPalette.swift
@@ -20,15 +20,12 @@ extension Theme.Palette {
         palette.textfieldButtonBackgroundColor = .white
         palette.navigationBarBackIcon = Asset.iconBack.image
         palette.brandBackground = Macros.color(hex: 0x009a18, alpha: 0xff)
-        palette.lightBackground = .white
-        palette.solidLightBackground = .piaGrey1
+        palette.secondaryBackground = .white
+        palette.principalBackground = .piaGrey1
         palette.lineColor = .piaGreenDark20
-        palette.subtitleColor = .piaGrey8
         palette.emphasis = Macros.color(hex: 0x29cc41, alpha: 0xff)
         palette.accent1 = UIColor.piaOrange
         palette.accent2 = Macros.color(hex: 0xe60924, alpha: 0xff)
-        palette.setDarkText(Macros.color(hex: 0x001b31, alpha: 0xff), alphas: [0.87, 0.67, 0.37])
-        palette.setLightText(.white, alphas: [0.87, 0.67, 0.37])
         palette.divider = UIColor(white: 0.0, alpha: 0.2)
         palette.overlayAlpha = 0.3
         return palette
