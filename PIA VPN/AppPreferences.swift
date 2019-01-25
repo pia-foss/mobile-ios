1ef6d94e109c41205be02f309f952d6bca925398
Jose Blaya <ueshiba@gmail.com>
2019-01-25 18:46:12 +0100

    Migrate existing users with a valid token

--- PIA VPN/AppPreferences.swift
+++ PIA VPN/AppPreferences.swift
@@ -32,7 +32,7 @@ class AppPreferences {
 
     static let shared = AppPreferences()
     
-    private static let currentVersion = "4.0"
+    private static let currentVersion = "5.0"
     
     private let defaults: UserDefaults
 
@@ -118,6 +118,12 @@ class AppPreferences {
         ])
     }
     
+    private func refreshAPIToken() {
+        if Client.providers.accountProvider.isLoggedIn {
+            Client.providers.accountProvider.refreshAndLogoutUnauthorized(force: true)
+        }
+    }
+    
     private func migrateAPItoV2() {
         // Migrate users from v1 to v2
         log.debug("Migration to api v2")
@@ -137,9 +143,11 @@ class AppPreferences {
     func migrate() {
         let oldVersion = defaults.string(forKey: Entries.version)
         defaults.set(AppPreferences.currentVersion, forKey: Entries.version)
-        
+
         guard (oldVersion == nil) else {
-            if oldVersion != "4.0" {
+            if oldVersion == "4.0" {
+                refreshAPIToken()
+            } else if oldVersion != "4.0" && oldVersion != AppPreferences.currentVersion {
                 migrateAPItoV2()
             }
             return
--- Podfile
+++ Podfile
@@ -53,7 +53,7 @@ end
 def shared_main_pods
     pod 'AlamofireImage'
     #library_by_path('')
-    library_by_git('49f14db')
+    library_by_git('dd71780')
     #library_by_version('~> 1.1.3')
 end
 
