868d5599f0fd2527b48b8be42e3dfdbd8666e90c
Jose Blaya <ueshiba@gmail.com>
2019-03-21 13:49:56 +0100

    Refactor

---
+++ PIA VPN/DashboardCollectionViewUtil.swift
@@ -0,0 +1,74 @@
+//
+//  DashboardCollectionViewUtil.swift
+//  PIA VPN
+//
+//  Created by Jose Antonio Blaya Garcia on 21/03/2019.
+//  Copyright © 2019 London Trust Media. All rights reserved.
+//
+
+import UIKit
+
+enum Cells: Int, EnumsBuilder {
+    
+    case region = 0
+    case quickConnect
+    case ipTile
+    case subscription
+    case usage
+    case networkManagementTool
+    case quickSettings
+    
+    var identifier: String {
+        switch self {
+        case .ipTile: return "IPTileCell"
+        case .quickConnect: return "QuickConnectTileCell"
+        case .region: return "RegionTileCell"
+        case .subscription: return "SubscriptionTileCell"
+        case .usage: return "UsageTileCell"
+        case .networkManagementTool: return "NMTTileCell"
+        case .quickSettings: return "QuickSettingsTileCell"
+        }
+    }
+    
+    var className: String {
+        switch self {
+        case .ipTile: return "IPTileCollectionViewCell"
+        case .quickConnect: return "QuickConnectTileCollectionViewCell"
+        case .region: return "RegionTileCollectionViewCell"
+        case .subscription: return "SubscriptionTileCollectionViewCell"
+        case .usage: return "UsageTileCollectionViewCell"
+        case .networkManagementTool: return "NetworkManagementToolTileCollectionViewCell"
+        case .quickSettings: return "QuickSettingsTileCollectionViewCell"
+        }
+    }
+}
+
+class DashboardCollectionViewUtil: NSObject {
+
+    func registerCellsFor(_ collectionView: UICollectionView) {
+        collectionView.register(UINib(nibName: Cells.ipTile.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.ipTile.identifier)
+        collectionView.register(UINib(nibName: Cells.quickConnect.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.quickConnect.identifier)
+        collectionView.register(UINib(nibName: Cells.region.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.region.identifier)
+        collectionView.register(UINib(nibName: Cells.subscription.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.subscription.identifier)
+        collectionView.register(UINib(nibName: Cells.usage.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.usage.identifier)
+        collectionView.register(UINib(nibName: Cells.networkManagementTool.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.networkManagementTool.identifier)
+        collectionView.register(UINib(nibName: Cells.quickSettings.className,
+                                      bundle: nil),
+                                forCellWithReuseIdentifier: Cells.quickSettings.identifier)
+        collectionView.backgroundColor = .clear
+    }
+    
+
+}
