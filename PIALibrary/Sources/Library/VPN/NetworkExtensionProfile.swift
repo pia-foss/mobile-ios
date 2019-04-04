3363d2d59fe20c5f25de93784da3ea174df5bc4f
Jose Blaya <ueshiba@gmail.com>
2019-04-04 12:52:13 +0200

    - Fix issue reconnecting VPN with all NMT options enabled

--- PIALibrary/Sources/Library/VPN/NetworkExtensionProfile.swift
+++ PIALibrary/Sources/Library/VPN/NetworkExtensionProfile.swift
@@ -65,71 +65,17 @@ extension NetworkExtensionProfile {
         if vpn.isOnDemandEnabled {
             
             if Client.preferences.nmtRulesEnabled {
-                
-                if Client.preferences.useWiFiProtection {
-                
-                    let ruleDisconnect = NEOnDemandRuleDisconnect()
-                    ruleDisconnect.interfaceTypeMatch = .wiFi
-                    if let currentSSID = UIDevice.current.WiFiSSID {
-                        let filteredNetworkList = trustedNetworks.filter({
-                            return $0 != currentSSID
-                        })
-                        ruleDisconnect.ssidMatch = filteredNetworkList
-                        let ruleIgnore = NEOnDemandRuleIgnore()
-                        ruleIgnore.interfaceTypeMatch = .wiFi
-                        ruleIgnore.ssidMatch = [currentSSID]
-                        vpn.onDemandRules?.append(ruleIgnore)
-                    } else {
-                        ruleDisconnect.ssidMatch = trustedNetworks
-                    }
-                    
-                    let ruleConnect = NEOnDemandRuleConnect()
-                    ruleConnect.interfaceTypeMatch = .wiFi
-                    if Client.preferences.shouldConnectForAllNetworks ||
-                        ruleDisconnect.ssidMatch?.count == 0 {
-                        vpn.onDemandRules?.append(ruleConnect)
-                    } else {
-                        vpn.onDemandRules?.append(contentsOf: [ruleDisconnect, ruleConnect])
-                    }
-                } else {
-                    if let _ = UIDevice.current.WiFiSSID { //If trying to connect from a WiFi network...
-                        let ruleIgnore = NEOnDemandRuleIgnore()
-                        ruleIgnore.interfaceTypeMatch = .wiFi
-                        vpn.onDemandRules?.append(ruleIgnore)
-                    } else {
-                        let ruleDisconnect = NEOnDemandRuleDisconnect()
-                        ruleDisconnect.interfaceTypeMatch = .wiFi
-                        vpn.onDemandRules?.append(ruleDisconnect)
-                    }
-                }
-                if !Client.preferences.trustCellularData {
-                    let ruleConnect = NEOnDemandRuleConnect()
-                    ruleConnect.interfaceTypeMatch = .cellular
-                    vpn.onDemandRules?.append(ruleConnect)
-                } else {
-                    if let _ = UIDevice.current.WiFiSSID { //If trying to connect from a Cellular network...
-                        let ruleDisconnect = NEOnDemandRuleDisconnect()
-                        ruleDisconnect.interfaceTypeMatch = .cellular
-                        vpn.onDemandRules?.append(ruleDisconnect)
-                    } else {
-                        let ruleIgnore = NEOnDemandRuleIgnore()
-                        ruleIgnore.interfaceTypeMatch = .cellular
-                        vpn.onDemandRules?.append(ruleIgnore)
-                    }
-                }
+                log.debug("Network Management Rule Enabled: \(Client.preferences.nmtRulesEnabled)")
+                log.debug("Network Management Rule Protect WiFi Network: \(Client.preferences.useWiFiProtection)")
+                log.debug("Network Management Rule Trust Cellular Network: \(Client.preferences.trustCellularData)")
+                log.debug("Network Management Rule Protect all WiFi Networks: \(Client.preferences.shouldConnectForAllNetworks)")
+                log.debug("Network Management Rule Trusted Networks: \(Client.preferences.trustedNetworks)")
+                self.configureOnDemandOnWiFiNetworksFor(trustedNetworks, vpn)
+                self.configureOnDemandOnCellularNetworks(vpn)
             } else {
-                
-                if force {
-                    vpn.isOnDemandEnabled = configuration.isOnDemand
-                } else {
-                    vpn.isOnDemandEnabled = vpn.isOnDemandEnabled && configuration.isOnDemand
-                }
-                if vpn.isOnDemandEnabled {
-                    vpn.onDemandRules = [NEOnDemandRuleConnect()]
-                }
-
+                log.debug("Network Management Tool is not enabled")
+                self.configureDefaultOnDemandRules(force, vpn, configuration)
             }
-            
         }
         
         log.debug("Configured with server: \(protocolConfiguration.serverAddress!)")
@@ -148,4 +94,80 @@ extension NetworkExtensionProfile {
             }
         }
     }
+    
+    
+    private func configureOnDemandOnWiFiNetworksFor(_ trustedNetworks: [String],
+                                                    _ vpn: NEVPNManager) {
+        if Client.preferences.useWiFiProtection {
+            
+            let ruleDisconnect = NEOnDemandRuleDisconnect()
+            ruleDisconnect.interfaceTypeMatch = .wiFi
+            if let currentSSID = UIDevice.current.WiFiSSID {
+                let filteredNetworkList = trustedNetworks.filter({
+                    return $0 != currentSSID
+                })
+                ruleDisconnect.ssidMatch = filteredNetworkList
+                let ruleIgnore = NEOnDemandRuleIgnore()
+                ruleIgnore.interfaceTypeMatch = .wiFi
+                ruleIgnore.ssidMatch = [currentSSID]
+                if trustedNetworks.contains(currentSSID) {
+                    vpn.onDemandRules?.append(ruleIgnore) //Only add the ignore rule if the current WiFi network is a trusted network
+                }
+            } else {
+                ruleDisconnect.ssidMatch = trustedNetworks
+            }
+            
+            let ruleConnect = NEOnDemandRuleConnect()
+            ruleConnect.interfaceTypeMatch = .wiFi
+            if Client.preferences.shouldConnectForAllNetworks {
+                vpn.onDemandRules = [ruleConnect]
+            } else if ruleDisconnect.ssidMatch?.count == 0 {
+                vpn.onDemandRules?.append(ruleConnect)
+            } else {
+                vpn.onDemandRules?.append(contentsOf: [ruleDisconnect, ruleConnect])
+            }
+        } else {
+            if let _ = UIDevice.current.WiFiSSID { //If trying to connect from a WiFi network...
+                let ruleIgnore = NEOnDemandRuleIgnore()
+                ruleIgnore.interfaceTypeMatch = .wiFi
+                vpn.onDemandRules?.append(ruleIgnore)
+            } else {
+                let ruleDisconnect = NEOnDemandRuleDisconnect()
+                ruleDisconnect.interfaceTypeMatch = .wiFi
+                vpn.onDemandRules?.append(ruleDisconnect)
+            }
+        }
+    }
+    
+    private func configureOnDemandOnCellularNetworks(_ vpn: NEVPNManager) {
+        if !Client.preferences.trustCellularData {
+            let ruleConnect = NEOnDemandRuleConnect()
+            ruleConnect.interfaceTypeMatch = .cellular
+            vpn.onDemandRules?.append(ruleConnect)
+        } else {
+            if let _ = UIDevice.current.WiFiSSID { //If trying to connect from a Cellular network...
+                let ruleDisconnect = NEOnDemandRuleDisconnect()
+                ruleDisconnect.interfaceTypeMatch = .cellular
+                vpn.onDemandRules?.append(ruleDisconnect)
+            } else {
+                let ruleIgnore = NEOnDemandRuleIgnore()
+                ruleIgnore.interfaceTypeMatch = .cellular
+                vpn.onDemandRules?.append(ruleIgnore)
+            }
+        }
+    }
+    
+    private func configureDefaultOnDemandRules(_ force: Bool,
+                                               _ vpn: NEVPNManager,
+                                               _ configuration: VPNConfiguration) {
+        if force {
+            vpn.isOnDemandEnabled = configuration.isOnDemand
+        } else {
+            vpn.isOnDemandEnabled = vpn.isOnDemandEnabled && configuration.isOnDemand
+        }
+        if vpn.isOnDemandEnabled {
+            vpn.onDemandRules = [NEOnDemandRuleConnect()]
+        }
+    }
+    
 }
