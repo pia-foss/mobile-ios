1ed8faf20b9e588c2e16a18f6528b5312e58f58f
Jose Blaya <ueshiba@gmail.com>
2020-06-12 12:50:35 +0200

    Pushed missed code from Array+Group

--- PIA VPN/Array+Group.swift
+++ PIA VPN/Array+Group.swift
@@ -20,3 +20,10 @@
 //
 
 import Foundation
+
+extension Array {
+   func filterDuplicate<T>(_ keyValue:(Element)->T) -> [Element] {
+      var uniqueKeys = Set<String>()
+      return filter{uniqueKeys.insert("\(keyValue($0))").inserted}
+   }
+}
