c6464cda1a437bc9469aa8b43212aa7fbe69f7b7
Jose Blaya <ueshiba@gmail.com>
2020-07-16 10:52:08 +0200

    wip

---
+++ PIA VPN/Tiles/ConnectionTile.swift
@@ -0,0 +1,70 @@
+//
+//  ConnectionTile.swift
+//  PIA VPN
+//  
+//  Created by Jose Blaya on 16/07/2020.
+//  Copyright © 2020 Private Internet Access, Inc.
+//
+//  This file is part of the Private Internet Access iOS Client.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software 
+//  without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+//  permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+//  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+//
+
+import UIKit
+import PIALibrary
+
+class ConnectionTile: UIView, Tileable  {
+    
+    var view: UIView!
+    var detailSegueIdentifier: String!
+    var status: TileStatus = .normal {
+        didSet {
+            statusUpdated()
+        }
+    }
+
+    @IBOutlet private weak var tileTitle: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.xibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.xibSetup()
+        self.setupView()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func setupView() {
+        
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(viewShouldRestyle), name: .PIAThemeDidChange, object: nil)
+
+        viewShouldRestyle()
+        self.tileTitle.text = "Connection Tile".uppercased()
+
+    }
+    
+    @objc private func viewShouldRestyle() {
+        tileTitle.style(style: TextStyle.textStyle21)
+        Theme.current.applyPrincipalBackground(self)
+    }
+    
+    private func statusUpdated() {
+    }
+    
+}
