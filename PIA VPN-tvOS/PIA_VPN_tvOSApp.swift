1f6ddfbd337e9b6bdebc8099a829e2279cd5a099
kp-said-rehouni <109279805+kp-said-rehouni@users.noreply.github.com>
2023-11-23 12:27:53 +0100

    PIA-845: Create basic target for tvOS (#40)
    * PIA-845: Create basic target for tvOS
    
    * PIA-845: Disable parallel E2E testing

---
+++ PIA VPN-tvOS/PIA_VPN_tvOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  PIA_VPN_tvOSApp.swift
+//  PIA VPN-tvOS
+//
+//  Created by Said Rehouni on 22/11/23.
+//  Copyright © 2023 Private Internet Access Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PIA_VPN_tvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
