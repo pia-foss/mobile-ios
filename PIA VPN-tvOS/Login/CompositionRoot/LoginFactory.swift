99251039a130695b5b094b15f6d6091468a456b0
Said Rehouni <said.rehouni@kape.com>
2023-12-11 22:30:06 +0100

    PIA-882: Add Login factory to create Login feature

---
+++ PIA VPN-tvOS/Login/CompositionRoot/LoginFactory.swift
@@ -0,0 +1,32 @@
+//
+//  LoginFactory.swift
+//  PIA VPN-tvOS
+//
+//  Created by Said Rehouni on 4/12/23.
+//  Copyright © 2023 Private Internet Access Inc. All rights reserved.
+//
+
+import Foundation
+import PIALibrary
+
+class LoginFactory {
+    static func makeLoginView() -> LoginView {
+        LoginView(viewModel: makeLoginViewModel())
+    }
+    
+    private static func makeLoginViewModel() -> LoginViewModel {
+        LoginViewModel(loginWithCredentialsUseCase: makeLoginWithCredentialsUseCase(),
+                       checkLoginAvailability: CheckLoginAvailability(),
+                       validateLoginCredentials: ValidateCredentialsFormat(),
+                       errorMapper: LoginPresentableErrorMapper())
+    }
+    
+    private static func makeLoginWithCredentialsUseCase() -> LoginWithCredentialsUseCaseType {
+        LoginWithCredentialsUseCase(loginProvider: makeLoginProvider(),
+                                    errorMapper: LoginDomainErrorMapper())
+    }
+    
+    private static func makeLoginProvider() -> LoginProviderType {
+        LoginProvider(accountProvider: Client.providers.accountProvider)
+    }
+}
