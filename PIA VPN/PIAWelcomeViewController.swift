d37e6881f8bb07cbaf1e43cc737fccf09855fe0b
Diego Trevisan <diego.trevisan@kape.com>
2025-11-13 10:52:18 +0100

    Remove traitCollection override to fix black screen/crash on iPads

--- PIA VPN/PIAWelcomeViewController.swift
+++ PIA VPN/PIAWelcomeViewController.swift
@@ -225,18 +225,7 @@ public class PIAWelcomeViewController: AutolayoutViewController, WelcomeCompleti
             }
         }
     }
-    
-    // MARK: Size classes
-    
-    // consider compact height in landscape
-    /// :nodoc:
-    public override var traitCollection: UITraitCollection {
-        if isLandscape {
-            return UITraitCollection(verticalSizeClass: .compact)
-        }
-        return super.traitCollection
-    }
-    
+
     // MARK: Restylable
     
     /// :nodoc:
