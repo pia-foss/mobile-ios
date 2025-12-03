c8b8bdf5f1000e3959bd8a9868904a0d7d96ec5b
Diego Trevisan <diego.trevisan@kape.com>
2025-12-03 15:00:00 +0100

    KM-13167 Display confirmation alert when changing server while already connected (#197)

--- PIA VPN/DashboardViewController+ServerSelection.swift
+++ PIA VPN/DashboardViewController+ServerSelection.swift
@@ -11,14 +11,34 @@ import PIALibrary
 extension DashboardViewController: ServerSelectionDelegate {
     
     func didSelectServer(_ server: Server) {
-        
         let isConnected = Client.providers.vpnProvider.isVPNConnected
         let currentServer = Client.preferences.displayedServer
+
         if isConnected {
             guard (server.identifier != currentServer.identifier || server.dipToken != currentServer.dipToken) else {
                 return
             }
+
+            let alert = Macros.alert(
+                L10n.Localizable.Dashboard.Vpn.ChangeLocation.Alert.title,
+                L10n.Localizable.Dashboard.Vpn.ChangeLocation.Alert.message
+            )
+
+            // reconnect -> reconnect VPN and close
+            alert.addActionWithTitle(L10n.Localizable.Settings.Commit.Buttons.reconnect) {
+                self.connect(to: server)
+            }
+
+            // cancel -> do nothing
+            alert.addCancelActionWithTitle(L10n.Localizable.Global.cancel) {}
+
+            present(alert, animated: true)
+        } else {
+            connect(to: server)
         }
+    }
+
+    private func connect(to server: Server) {
         if TrustedNetworkUtils.isTrustedNetwork {
             showAutomationAlert() {
                 Client.configuration.connectedManually = true
