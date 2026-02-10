eaed9b11e00089b55393b3ca745e1f48274f1c64
Nils Crefeld <nils.crefeld@kape.com>
2026-02-10 18:15:57 +0100

    Add PIAAccount native library to replace the KMP library

---
+++ LocalPackages/PIAAccount/Sources/PIAAccount/Networking/APIPath.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+/// API endpoint paths
+enum APIPath: String, Sendable {
+    case login = "/api/client/v5/api_token"
+    case vpnToken = "/api/client/v5/vpn_token"
+    case refreshAPIToken = "/api/client/v5/refresh"
+    case signup = "/api/client/signup"
+    case setEmail = "/api/client/account"
+    case loginLink = "/api/client/v2/login_link"
+    case logout = "/api/client/v2/expire_token"
+    case accountDetails = "/api/client/v2/account"
+    case deleteAccount = "/api/client/v5/account"
+    case clientStatus = "/api/client/status"
+    case invites = "/api/client/invites"
+    case redeem = "/api/client/giftcard_redeem"
+    case refreshToken = "/api/client/v4/refresh"
+    case messages = "/api/client/v2/messages"
+    case dedicatedIP = "/api/client/v2/dedicated_ip"
+    case renewDedicatedIP = "/api/client/v2/check_renew_dip"
+    case iosPayment = "/api/client/payment"
+    case iosSubscriptions = "/api/client/ios"
+    case iosFeatureFlag = "/clients/desktop/ios-flags"
+
+    /// Returns the subdomain for this API path
+    var subdomain: String {
+        switch self {
+        case .login, .vpnToken, .refreshAPIToken:
+            return "apiv5"
+        case .loginLink, .logout, .accountDetails, .messages, .dedicatedIP, .renewDedicatedIP:
+            return "apiv2"
+        case .deleteAccount:
+            return "apiv5"
+        case .refreshToken:
+            return "apiv4"
+        case .signup, .setEmail, .clientStatus, .invites, .redeem, .iosPayment, .iosSubscriptions, .iosFeatureFlag:
+            return "api"
+        }
+    }
+}
