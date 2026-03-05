421eda717442c7553d2169c5245fbb0a780a87b9
Diego Trevisan <diego.trevisan@kape.com>
2026-03-05 15:36:47 +0100

    KM-14522 Update mobile-ios-openvpn and mobile-ios-wireguard

--- LocalPackages/PIALibrary/Package.swift
+++ LocalPackages/PIALibrary/Package.swift
@@ -21,10 +21,10 @@ let package = Package(
       .package(url: "git@github.com:pia-foss/mobile-ios-releases-account.git", exact: "1.4.5"),
       .package(url: "git@github.com:pia-foss/mobile-ios-releases-regions.git", exact: "1.6.3"),
       .package(url: "git@github.com:pia-foss/mobile-ios-openvpn.git", branch: "master"),
-      .package(url: "git@github.com:pia-foss/mobile-ios-wireguard.git", revision: "bf7b4258d9c9279c6051bba0b7a73ca7d5f9547e"),
+      .package(url: "git@github.com:pia-foss/mobile-ios-wireguard.git", exact: "1.0.2"),
+      .package(url: "git@github.com:pia-foss/mobile-ios-networking.git", exact: "1.3.1"),
       .package(url: "https://github.com/apple/swift-log", exact: "1.10.1"),
-      .package(url: "https://github.com/ashleymills/Reachability.swift.git", exact: "5.2.4"),
-      .package(url: "git@github.com:pia-foss/mobile-ios-networking.git", exact: "1.3.1")
+      .package(url: "https://github.com/ashleymills/Reachability.swift.git", exact: "5.2.4")
     ],
     targets: [
         .target(
