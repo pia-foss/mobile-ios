3df2a63b903697c537c78495f2e75b8037ff4729
Diego Trevisan <diego.trevisan@kape.com>
2026-04-02 15:45:33 +0200

    KM-15263 Add PIACSI local Swift package

---
+++ LocalPackages/PIACSI/Sources/PIACSI/CSIDataProvider.swift
@@ -0,0 +1,28 @@
+//
+//  CSIDataProvider.swift
+//  PIACSI
+//
+//  Created by Diego Trevisan on 02.04.26.
+//  Copyright © 2026 Private Internet Access, Inc.
+//
+//  This file is part of the Private Internet Access iOS Client.
+//
+//  The Private Internet Access iOS Client is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The Private Internet Access iOS Client is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License along with the Private
+//  Internet Access iOS Client.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+public protocol CSIDataProvider {
+    var sectionName: String { get }
+    var content: String? { get }
+}
---
+++ LocalPackages/PIACSI/Sources/PIACSI/CSIResponse.swift
@@ -0,0 +1,36 @@
+//
+//  CSIResponse.swift
+//  PIACSI
+//
+//  Created by Diego Trevisan on 02.04.26.
+//  Copyright © 2026 Private Internet Access, Inc.
+//
+//  This file is part of the Private Internet Access iOS Client.
+//
+//  The Private Internet Access iOS Client is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The Private Internet Access iOS Client is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License along with the Private
+//  Internet Access iOS Client.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+struct CreateResponse: Codable {
+    let result: String
+    let code: String?
+    let message: String?
+    var isSuccess: Bool { result == "success" }
+}
+
+struct BaseResponse: Codable {
+    let result: String
+    let message: String?
+    var isSuccess: Bool { result == "success" }
+}
