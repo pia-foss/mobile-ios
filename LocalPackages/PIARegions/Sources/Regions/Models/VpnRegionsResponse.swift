d5f32e85ef12dca5616e4b3571457a4f1cc93e26
Diego Trevisan <diego.trevisan@kape.com>
2026-04-07 10:00:57 +0200

    KM-15346 Add PIARegions local Swift package

---
+++ LocalPackages/PIARegions/Sources/Regions/Models/VpnRegionsResponse.swift
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (c) 2020 Private Internet Access, Inc.
+ *
+ *  This file is part of the Private Internet Access Mobile Client.
+ *
+ *  The Private Internet Access Mobile Client is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  The Private Internet Access Mobile Client is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along with the Private
+ *  Internet Access Mobile Client.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+
+
+public struct VPNRegionsResponse: Codable, Sendable {
+    public var groups: [String: [ProtocolDetails]]
+    public var regions: [Region]
+
+    public init(groups: [String: [ProtocolDetails]] = [:], regions: [Region] = []) {
+        self.groups = groups
+        self.regions = regions
+    }
+
+    public struct ProtocolDetails: Codable, Sendable {
+        public var name: String
+        public var ports: [Int]
+
+        public init(name: String = "", ports: [Int] = []) {
+            self.name = name
+            self.ports = ports
+        }
+    }
+
+    public struct Region: Codable, Sendable {
+        public var id: String
+        public var name: String
+        public var country: String
+        public var dns: String
+        public var geo: Bool
+        public var offline: Bool
+        public var latitude: String?
+        public var longitude: String?
+        public var autoRegion: Bool
+        public var portForward: Bool
+        public var servers: [String: [ServerDetails]]
+
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case country
+            case dns
+            case geo
+            case offline
+            case latitude
+            case longitude
+            case autoRegion = "auto_region"
+            case portForward = "port_forward"
+            case servers
+        }
+
+        public init(
+            id: String = "",
+            name: String = "",
+            country: String = "",
+            dns: String = "",
+            geo: Bool = false,
+            offline: Bool = false,
+            latitude: String? = nil,
+            longitude: String? = nil,
+            autoRegion: Bool = false,
+            portForward: Bool = false,
+            servers: [String: [ServerDetails]] = [:]
+        ) {
+            self.id = id
+            self.name = name
+            self.country = country
+            self.dns = dns
+            self.geo = geo
+            self.offline = offline
+            self.latitude = latitude
+            self.longitude = longitude
+            self.autoRegion = autoRegion
+            self.portForward = portForward
+            self.servers = servers
+        }
+
+        public struct ServerDetails: Codable, Sendable {
+            public var ip: String
+            public var cn: String
+
+            public init(ip: String = "", cn: String = "") {
+                self.ip = ip
+                self.cn = cn
+            }
+        }
+    }
+}
---
+++ LocalPackages/PIARegions/Sources/Regions/RegionsUtils.swift
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2020 Private Internet Access, Inc.
+ *
+ *  This file is part of the Private Internet Access Mobile Client.
+ *
+ *  The Private Internet Access Mobile Client is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  The Private Internet Access Mobile Client is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along with the Private
+ *  Internet Access Mobile Client.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+
+
+public enum RegionsUtils {
+
+    /// Encodes a `VPNRegionsResponse` to a JSON string.
+    public static func stringify(_ response: VPNRegionsResponse) throws -> String {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(response)
+        guard let json = String(data: data, encoding: .utf8) else {
+            throw RegionsUtilsError.encodingFailed
+        }
+        return json
+    }
+
+    /// Decodes a `VPNRegionsResponse` from a JSON string.
+    public static func parse(_ json: String) throws -> VPNRegionsResponse {
+        guard let data = json.data(using: .utf8) else {
+            throw RegionsUtilsError.invalidInput
+        }
+        let decoder = JSONDecoder()
+        return try decoder.decode(VPNRegionsResponse.self, from: data)
+    }
+
+    internal static func isErrorStatusCode(_ code: Int) -> Bool {
+        switch code {
+        case 300...399:
+            // Redirect response
+            return true
+        case 400...499:
+            // Client error response
+            return true
+        case 500...599:
+            // Server error response
+            return true
+        default:
+            if code >= 600 {
+                // Unknown error response
+                return true
+            }
+            return false
+        }
+    }
+}
+
+public enum RegionsUtilsError: Error, Sendable {
+    case encodingFailed
+    case invalidInput
+}
