867f19e52afc82cda6e3fa009fbf5b7cf53a4841
Mario Nachbaur Riscos <mario.nachbaur@kape.com>
2026-07-07 12:06:40 +0200

    KM-16629: fix tvOS test target

--- PIA VPN-tvOSTests/Signup/Mocks/InAppProviderSpy.swift
+++ PIA VPN-tvOSTests/Signup/Mocks/InAppProviderSpy.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import PIABase
 import PIALibrary
 import StoreKit
 
--- PIA VPN-tvOSTests/Signup/Mocks/InAppTransactionMock.swift
+++ PIA VPN-tvOSTests/Signup/Mocks/InAppTransactionMock.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import PIABase
 import PIALibrary
 
 class InAppTransactionMock: InAppTransaction {
